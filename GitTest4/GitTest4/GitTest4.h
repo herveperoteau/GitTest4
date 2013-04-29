479f9b07dfa53b871b3dbd2eda6f19ef29c52802
Herve Peroteau <herve.peroteau@gmail.com>
2013-04-29 07:11:40 +0200

    v1

---
+++ GitTest4/GitTest4/GitTest4.h
@@ -0,0 +1,13 @@
+//
+//  GitTest4.h
+//  GitTest4
+//
+//  Created by Hervé PEROTEAU on 29/04/13.
+//  Copyright (c) 2013 Hervé PEROTEAU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GitTest4 : NSObject
+
+@end
---
+++ GitTest4/GitTest4/GitTest4.m
@@ -0,0 +1,13 @@
+//
+//  GitTest4.m
+//  GitTest4
+//
+//  Created by Hervé PEROTEAU on 29/04/13.
+//  Copyright (c) 2013 Hervé PEROTEAU. All rights reserved.
+//
+
+#import "GitTest4.h"
+
+@implementation GitTest4
+
+@end
